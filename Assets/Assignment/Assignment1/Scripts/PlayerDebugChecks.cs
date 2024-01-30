8bd3bbc630c1b424bbb8f04625fb4c85cecb1f54
ROTOM\chris <salamec@sheridancollege.ca>
2024-01-30 14:39:18 -0500

    changed on trigger

--- Assets/Assignment/Assignment1/Scripts/PlayerDebugChecks.cs
+++ Assets/Assignment/Assignment1/Scripts/PlayerDebugChecks.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpikeCheck : MonoBehaviour
+public class PlayerDebugChecks : MonoBehaviour
 {
-    public SpriteRenderer Sensor;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -16,7 +15,6 @@ public class SpikeCheck : MonoBehaviour
     {
         
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject + "Hitting Checkpoint");
